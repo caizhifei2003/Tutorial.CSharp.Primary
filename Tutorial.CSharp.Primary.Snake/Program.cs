05caa3b9d59fab541c3c9975cd776df0f2290754
Kevin Cai <caizhifei2003@163.com>
2019-06-10 17:35:53 +0800

    update

--- Tutorial.CSharp.Primary.Snake/Program.cs
+++ Tutorial.CSharp.Primary.Snake/Program.cs
@@ -5,19 +5,29 @@ namespace Tutorial.CSharp.Primary.Snake {
         const char CH_PANEL = '□';
         const char CH_HEAD = '※';
         const char CH_BODY = '⊙';
+        const char CH_BEAN = '⊙';
+
+        const ConsoleColor COLOR_PANEL = ConsoleColor.White;
+        const ConsoleColor COLOR_HEAD = ConsoleColor.Blue;
+        const ConsoleColor COLOR_BODY = ConsoleColor.Yellow;
+        const ConsoleColor COLOR_BEAN = ConsoleColor.Red;
 
         static int rows = 20;
         static int columns = 20;
 
         static int head_x = 0;
-        static int head_y = 1;
+        static int head_y = 0;
 
         static int[] body_x;
         static int[] body_y;
 
+        static int? bean_x = null;
+        static int? bean_y = null;
+
         static void Main(string[] args) {
             Console.CursorVisible = false;
-            DrawPanel();
+            GenBean();
+            Draw();
 
             for (var key = Console.ReadKey(); key.Key != ConsoleKey.Escape; key = Console.ReadKey()) {
                 switch (key.Key) {
@@ -28,18 +38,63 @@ namespace Tutorial.CSharp.Primary.Snake {
                     default: break;
                 }
 
-                DrawPanel();
+                Draw();
             }
         }
 
+        static void GenBean() {
+            var rand = new Random();
+            bean_x = rand.Next(columns);
+            bean_y = rand.Next(rows);
+        }
+
+        static void DrawHead() {
+            var old = Console.ForegroundColor;
+            Console.ForegroundColor = COLOR_HEAD;
+            Console.Write(CH_HEAD);
+            Console.ForegroundColor = old;
+        }
+
+        static void DrawBody() {
+            var old = Console.ForegroundColor;
+            Console.ForegroundColor = COLOR_BODY;
+            Console.Write(CH_BODY);
+            Console.ForegroundColor = old;
+        }
+
         static void DrawPanel() {
+            var old = Console.ForegroundColor;
+            Console.ForegroundColor = COLOR_PANEL;
+            Console.Write(CH_PANEL);
+            Console.ForegroundColor = old;
+        }
+
+        static void DrawBean() {
+            var old = Console.ForegroundColor;
+            Console.ForegroundColor = COLOR_BEAN;
+            Console.Write(CH_BEAN);
+            Console.ForegroundColor = old;
+        }
+
+        static void Draw() {
             Console.Clear();
-            for (var row = 0; row < rows; row++) {
-                for (var column = 0; column < columns; column++) {
-                    if (row == head_y && column == head_x) {
-                        Console.Write(CH_HEAD);
+            for (var y = 0; y < rows; y++) {
+                for (var x = 0; x < columns; x++) {
+                    if (y == head_y && x == head_x) {
+                        DrawHead();
+                    } else if (body_x != null && body_y != null) {
+                        for (var i = 0; i < body_x.Length; i++) {
+                            if (body_x[i] == x && body_y[i] == y) {
+                                DrawBody();
+                            }
+                        }
+                    } else if (bean_x != null
+                        && bean_x.Value == x
+                        && bean_y != null
+                        && bean_y.Value == y) {
+                        DrawBean();
                     } else {
-                        Console.Write(CH_PANEL);
+                        DrawPanel();
                     }
                 }
                 Console.WriteLine();
